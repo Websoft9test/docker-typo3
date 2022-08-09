967a040fe25d1d5604e69b0d688cb839f73d944c
qiaofeng1227 <76487013@qq.com>
2022-08-09 09:16:51 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -6,5 +6,7 @@ if [[ -f /var/www/html/wizard ]]; then
 else
     echo "" >> /var/www/html/wizard
     sed -i s/127.0.0.1/mysql/g  /var/www/html/typo3_src-11.5.12/typo3/sysext/install/Classes/Controller/InstallerController.php
+    cat InstallerController.php |grep username|grep TYPO3_CONF_VARS| awk '{print $NF}'
+    cat InstallerController.php |grep username|grep TYPO3_CONF_VARS| awk '{print $NF}'
 fi
 docker-php-entrypoint apache2-foreground
