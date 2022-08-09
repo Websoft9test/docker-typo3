d6a1e494496cd6e24870ccb536da67f3150324a0
qiaofeng1227 <76487013@qq.com>
2022-08-09 08:32:25 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # customrized cmd powered by Websoft9
 
-if [[ -f /www/server/panel/data/admin_path.pl ]]; then
-    
-else
+if [[ -f /var/www/html/wizard ]]; then
     echo "Initialization has been completed before this time!"
+else
+    echo "" >> /var/www/html/wizard
+    sed -i s/127.0.0.1/mysql/g  /var/www/html/typo3_src-11.5.12/typo3/sysext/install/Classes/Controller/InstallerController.php
 fi
 docker-php-entrypoint apache2-foreground
