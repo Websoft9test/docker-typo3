1baf97df45312134599a00635bb3d6f899f3a443
LalicNine <109707255+LalicNine@users.noreply.github.com>
2022-09-30 10:42:33 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -11,7 +11,7 @@ else
     cat InstallerController.php |grep username|grep TYPO3_CONF_VARS| awk '{print $NF}'
     
     sed -i "s/\['user'\] ?? ''/['user'] ?? '$TYPO3_DB_USER'/" /var/www/html/typo3_src-11.5.14/typo3/sysext/install/Classes/Controller/InstallerController.php
-    sed -i "s/\['host'\] ?? '127.0.0.1'/['password'] ?? '$TYPO3_DB_PASSWORD'/" /var/www/html/typo3_src-11.5.14/typo3/sysext/install/Classes/Controller/InstallerController.php
+    sed -i "s/\['password'\] ?? ''/['password'] ?? '$TYPO3_DB_PASSWORD'/" /var/www/html/typo3_src-11.5.14/typo3/sysext/install/Classes/Controller/InstallerController.php
     sed -i "s/\['host'\] ?? '127.0.0.1'/['host'] ?? '$TYPO3_DB_HOST'/" /var/www/html/typo3_src-11.5.14/typo3/sysext/install/Classes/Controller/InstallerController.php
 fi
 docker-php-entrypoint apache2-foreground
